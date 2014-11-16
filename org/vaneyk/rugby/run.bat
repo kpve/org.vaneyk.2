323070ec357e8a7eb267a8e859280e33d71b5acd
unknown <ken.vaneyk@gmail.com>
2014-11-15 17:19:06 -0800

    Bit of work on web sockets.

--- org/vaneyk/rugby/run.bat
+++ org/vaneyk/rugby/run.bat
@@ -3,3 +3,4 @@
 START CMD /C CALL .\.bat\run-rugby-data.bat %* ^& PAUSE
 START CMD /C CALL .\.bat\run-rugby-web.bat %* ^& PAUSE
 
+//
--- org/vaneyk/rugby/web/src/main/resources/static/script/application.js
+++ org/vaneyk/rugby/web/src/main/resources/static/script/application.js
@@ -2,13 +2,21 @@
 var app = angular.module( "app", [] );
 
 var helloController = function( $scope )
-       {
-           $scope.helloTo = {};
-           $scope.helloTo.title = "World, AngularJS";
-       };
+{
+    $scope.helloTo = {};
+    $scope.helloTo.title = "World, AngularJS";
+};
 
+var storyController = function( $scope )
+{
+    TODO
+};
+
+       
+       
 app.controller( "HelloController", helloController );
 
 
 
-HEY LOOK A THIS LINK TO START:  http://g00glen00b.be/spring-angular-sockjs/
+
+//HEY LOOK A THIS LINK TO START:  http://g00glen00b.be/spring-angular-sockjs/
